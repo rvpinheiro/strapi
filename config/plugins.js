6fbf10962ced908c306b13cf17253dcfa4a54353
Rui Pinheiro <rui.pinheiro@askblue.pt>
2024-12-19 17:42:59 +0000

    jwt secret key

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,7 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+        config: {
+            jwtSecret: env('JWT_SECRET', 'bTknjD74vnTg42zI+VskHehfI1Y+L2pC19F3wzHjksF8sbjzQwH7ZP4GzD5lhj3Z5A'),
+        },
+    },
+});
